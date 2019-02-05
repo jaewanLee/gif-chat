1c7c5490f6958653e2ed83ddad296dc7a2678e45
jaewanLee <ianlee9180@gmail.com>
2019-02-05 21:11:01 +0900

    [feature]router setting based on socket communication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,85 @@
 const express=require("express")
+
+const Room=require("../schemas/room")
+const Chat=require("../schemas/chat")
 const router=express.Router();
 
-router.get("/",(req,res)=>{
-  res.render('index')
+router.get("/",async (req,res,next)=>{
+  try{
+    const rooms=await Room.find({});
+    res.render("main",{rooms,title:"GIF chat room",error:req.flash("roomError")})
+  }
+  catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+router.get("/room",(req,res)=>{
+  res.render("/room",{title:"gif chat room created"})
+})
+router.post("/room",async (req,res,next)=>{
+  try{
+    const room=new Room({
+      title:req.body.title,
+      max:req.body.max,
+      owner:req.session.color,
+      password:req.body.password,
+    })
+    const newRoom=await room.save();
+    const io=req.app.get("io")
+    io.of("/room").emit("newRoom",newRoom);
+    res.redirect(`/room/${newRoom.id}?password=${req.body.password}`);
+  }catch (error) {
+    console.error(error)
+    next(error)
+
+  }
+
+  router.get(`/room/:id`,async (req,res,next)=>{
+    try{
+      const room=await Room.findOne({_id:req.params.id})
+      const io=req.app.get(`io`)
+      if(!room){
+        req.flash("roomError","not existed room");
+        return res.redirect("/")
+      }
+      if(room.password && room.password!=req.query.password){
+        req.flash("roomError","incorrect password")
+        return res.redirect("/")
+      }
+      const {rooms}=io.of("/chat").adapter;
+      if(rooms&&rooms[req.params.id]&&room.max<=rooms[req.params.id].length){
+        req.flash("roomError","over max number");
+        return res.redirect("/");
+      }
+      return res.render("chat",{
+        room,
+        title:room.title,
+        chats:[],
+        user:req.session.color
+      })
+    }
+    catch (error) {
+      console.error(error);
+      return next(error)
+
+    }
+  })
+
+  router.delete("/room/:id",async(req,res,next)=>{
+    try{
+      await Room.remove({_id:req.params.id})
+      await Chat.remove({room:req.params.id})
+      res.send("ok")
+      setTimeout(()=>{
+        req.app.get("io").of("/room").emit("removeRoom",req.params.id);
+      },2000);
+    }catch (error) {
+      console.error(error);
+      next(error)
+    }
+  })
 })
 
 module.exports=router;
